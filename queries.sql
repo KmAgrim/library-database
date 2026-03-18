4cd68e8c76af08eb5e0603422ef16698cc67443f
KmAgrim <agrim2004@proton.me>
2026-03-18 20:18:02 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,4 +1,12 @@
-SELECT loans.member_id, loans.book_id, members.name, books.title
+-- View all borrowing records with member names and book titles
+SELECT members.name, books.title
 FROM loans
 JOIN members ON loans.member_id = members.member_id
 JOIN books ON loans.book_id = books.book_id;
+
+-- View currently borrowed books (not yet returned)
+SELECT members.name, books.title
+FROM loans
+JOIN members ON loans.member_id = members.member_id
+JOIN books ON loans.book_id = books.book_id
+WHERE loans.return_date IS NULL;
