bce57c2e426b664e1a7bee933153d9a0f39ca016
KmAgrim <agrim2004@proton.me>
2026-03-18 20:29:45 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -10,3 +10,11 @@ FROM loans
 JOIN members ON loans.member_id = members.member_id
 JOIN books ON loans.book_id = books.book_id
 WHERE loans.return_date IS NULL;
+
+-- Find overdue books
+SELECT members.name, books.title, loans.due_date
+FROM loans
+JOIN members ON loans.member_id = members.member_id
+JOIN books ON loans.book_id = books.book_id
+WHERE loans.return_date IS NULL
+AND loans.due_date < CURDATE();
